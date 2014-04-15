9e7a25c79a638710203f8ccfa13fb06ff72c6d5e
Kevin Isom <kevin.isom@gmail.com>
2014-04-15 15:26:11 +1200

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-
 /**
  * Module dependencies.
  */
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
 
@@ -27,8 +25,7 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/', routes.index); 
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
